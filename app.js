090f91b2abeb175d4066c41d3c4895de814e92b3
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-03 12:12:02 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const express = require('express')
+const express = require('express');
+const Razorpay = require('razorpay');
+const path = require('path');
+const { validateWebhookSignature } = require('razorpay/dist/utils/razorpay-utils');
+const {error} = require('console');
 const app = express()
 const port = 3000
+
+Dotenv.config();
 
 
 app.use("view engine" , "ejs")
@@ -13,7 +19,19 @@ app.get('/', (req, res) => {
 
 // Creta Order Route
 app.post("/create-order", async(req,res) => {
-    
+    try {
+        const options ={
+            amount: req.body.amount,
+            currency: "INR",
+            receipt: "receipt_" + Date_Now(),
+        }
+
+        const order = await Razorpay.order.create(options)
+        res.json(order);
+        
+    } catch (error) {
+        res.status(500).send({error: error.message})
+    }
 })
 
 const PORT = process.env.PORT || 3000;
