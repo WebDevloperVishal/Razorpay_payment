ae932545a987a38dd970c857bd3881141bff1bfa
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-03 12:54:30 +0530

    commit

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const Razorpay = require('razorpay');
 const path = require('path');
+const Dotenv = require('dotenv'); 
 const { validateWebhookSignature } = require('razorpay/dist/utils/razorpay-utils');
-const {error} = require('console');
+const { error } = require('console');
 const app = express()
-const port = 3000
+
 
 Dotenv.config();
 
 
-app.use("view engine" , "ejs")
+app.set("view engine", "ejs")
 app.use(express.json())
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-    res.render('index', { key: process.env.RAZORPAY_KEY_ID})
+    res.render('index', { key: process.env.RAZORPAY_KEY_ID })
 })
 
 // Creta Order Route
-app.post("/create-order", async(req,res) => {
+app.post("/create-order", async (req, res) => {
     try {
-        const options ={
+        const options = {
             amount: req.body.amount,
             currency: "INR",
             receipt: "receipt_" + Date_Now(),
@@ -28,13 +29,38 @@ app.post("/create-order", async(req,res) => {
 
         const order = await Razorpay.order.create(options)
         res.json(order);
-        
+
     } catch (error) {
-        res.status(500).send({error: error.message})
+        res.status(500).send({ error: error.message })
     }
 })
 
+app.post("/verify-payment", (req, res) => {
+    try {
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+        const secret = process.env.RAZORPAY_KEY_SECRET;
+        const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+        const isValidSignature = validateWebhookSignature(body, razorpay_signature, secret)
+
+        if (isValidSignature) {
+            res.status(200).json({ status: "ok" })
+            console.log('Payment Verfication Successful');
+        } else {
+            res.status(400).json({ status: "Verfication Failed" })
+            console.log('Payment Verfication Failed');
+        }
+
+    } catch (err) {
+        res.send(500).send({ error: err.message })
+    }
+})
+
+app.get('/payment-success',  (req,res)=> {
+    res.sendFile(path.join(__dirname, "views/success.html"))
+})
+
 const PORT = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
-});
+})
